167bafec68f8337a44fd364a3738152c599a244e
Dayane Rodrigues <dayanealrodrigues@gmail.com>
2026-08-18 18:00:46 -0300

    Corrigindo campos nos scripts

--- banco/02_dados.sql
+++ banco/02_dados.sql
@@ -11,77 +11,248 @@ INSERT INTO hospedes (cpf,nome,email) VALUES
     ('90123456789', 'Isabela Martins Pereira', 'isabela.pereira@gmail.com'),
     ('01234567890', 'João Victor Mendes', 'joao.mendes@gmail.com');
 
+INSERT INTO telefones (numero, hospede_cpf) VALUES
+    ('51987654321', '00012320900'),
+    ('51991234567', '12345678901'),
+    ('51982345678', '23456789012'),
+    ('51993456789', '34567890123'),
+    ('51984567890', '45678901234'),
+    ('51995678901', '56789012345'),
+    ('51986789012', '67890123456'),
+    ('51997890123', '78901234567'),
+    ('51988901234', '89012345678'),
+    ('51999012345', '90123456789'),
+    ('51980123456', '01234567890');
+
 INSERT INTO tipos_de_quartos (nome,descricao) VALUES
     ('Individual', 'Quarto destinado para uma pessoa com uma cama de solteiro.'),
     ('Duplo', 'Quarto com duas camas de solteiro ou uma de casal.'),
     ('Triplo', 'Quarto com 3 camas de solteiro ou, uma de casal e uma de solteiro'),
     ('Suite',  'Quarto duplo com suite integrada');
 
-INSERT INTO quartos (capacidade,valor_diaria,status,tipo_id) VALUES
-    (1,70.00,'Disponível',1),
-    (1,70.00, 'Ocupado',1),
-    (1,70.00, 'Manutenção', 1),
-    (2, 110.00,'Disponível', 2),
-    (2, 110.00,'Ocupado', 2),
-    (2, 110.00,'Manutenção', 2),
-    (3, 130.00,'Disponível', 3),
-    (3, 130.00,'Ocupado', 3),
-    (2,150.00,'Disponível',4),
-    (2, 150.00,'Manutenção', 4);
+INSERT INTO quartos (capacidade, valor_diaria, status, tipo_id) VALUES
 
-INSERT INTO reservas (data_entrada, data_saida, quantidade_hospedes, status, observacao, hospede_cpf, quarto_id) VALUES
+    (1, 70.00, 'Disponível',
+        (SELECT id_tipo_de_quarto FROM tipos_de_quartos WHERE nome = 'Individual')),
+
+    (1, 70.00, 'Ocupado',
+        (SELECT id_tipo_de_quarto FROM tipos_de_quartos WHERE nome = 'Individual')),
+
+    (1, 70.00, 'Manutenção',
+        (SELECT id_tipo_de_quarto FROM tipos_de_quartos WHERE nome = 'Individual')),
+
+    (2, 110.00, 'Disponível',
+        (SELECT id_tipo_de_quarto FROM tipos_de_quartos WHERE nome = 'Duplo')),
+
+    (2, 110.00, 'Ocupado',
+        (SELECT id_tipo_de_quarto FROM tipos_de_quartos WHERE nome = 'Duplo')),
+
+    (2, 110.00, 'Manutenção',
+        (SELECT id_tipo_de_quarto FROM tipos_de_quartos WHERE nome = 'Duplo')),
+
+    (3, 130.00, 'Disponível',
+        (SELECT id_tipo_de_quarto FROM tipos_de_quartos WHERE nome = 'Triplo')),
+
+    (3, 130.00, 'Ocupado',
+        (SELECT id_tipo_de_quarto FROM tipos_de_quartos WHERE nome = 'Triplo')),
+
+    (2, 150.00, 'Disponível',
+        (SELECT id_tipo_de_quarto FROM tipos_de_quartos WHERE nome = 'Suite')),
+
+    (2, 150.00, 'Manutenção',
+        (SELECT id_tipo_de_quarto FROM tipos_de_quartos WHERE nome = 'Suite'));
+
+INSERT INTO reservas 
+(data_entrada, data_saida, quantidade_hospedes, status, observacao, hospede_cpf, quarto_id) 
+VALUES
+
+    -- Reserva 1 - Duplo
     (now() - INTERVAL '5 days', now() - INTERVAL '2 days', 2, 'Finalizada',
-    'Hospedagem de final de semana', '00012320900', 1),
+    'Hospedagem de final de semana', '00012320900',
+    (
+        SELECT q.id_quarto
+        FROM quartos q
+        JOIN tipos_de_quartos t 
+            ON t.id_tipo_de_quarto = q.tipo_id
+        WHERE t.nome = 'Duplo'
+        AND q.status = 'Ocupado'
+    )),
 
+    -- Reserva 2 - Individual
     (now() - INTERVAL '3 days', now() - INTERVAL '1 day', 1, 'Finalizada',
-    'Viagem a trabalho', '12345678901', 2),
+    'Viagem a trabalho', '12345678901',
+    (
+        SELECT q.id_quarto
+        FROM quartos q
+        JOIN tipos_de_quartos t 
+            ON t.id_tipo_de_quarto = q.tipo_id
+        WHERE t.nome = 'Individual'
+        AND q.status = 'Ocupado'
+    )),
 
+    -- Reserva 3 - Duplo
     (now() - INTERVAL '1 day', now() + INTERVAL '2 days', 2, 'Hospedado',
-    'Hospedagem com café da manhã', '23456789012', 3),
+    'Hospedagem com café da manhã', '23456789012',
+    (
+        SELECT q.id_quarto
+        FROM quartos q
+        JOIN tipos_de_quartos t 
+            ON t.id_tipo_de_quarto = q.tipo_id
+        WHERE t.nome = 'Duplo'
+        AND q.status = 'Disponível'
+    )),
 
+    -- Reserva 4 - Triplo
     (now() - INTERVAL '2 days', now() + INTERVAL '1 day', 3, 'Hospedado',
-    'Família em viagem', '34567890123', 4),
+    'Família em viagem', '34567890123',
+    (
+        SELECT q.id_quarto
+        FROM quartos q
+        JOIN tipos_de_quartos t 
+            ON t.id_tipo_de_quarto = q.tipo_id
+        WHERE t.nome = 'Triplo'
+        AND q.status = 'Ocupado'
+    )),
 
+    -- Reserva 5 - Individual
     (now() + INTERVAL '1 day', now() + INTERVAL '4 days', 1, 'Reservada',
-    'Reserva para viagem de trabalho', '45678901234', 1),
+    'Reserva para viagem de trabalho', '45678901234',
+    (
+        SELECT q.id_quarto
+        FROM quartos q
+        JOIN tipos_de_quartos t 
+            ON t.id_tipo_de_quarto = q.tipo_id
+        WHERE t.nome = 'Individual'
+        AND q.status = 'Disponível'
+    )),
 
+    -- Reserva 6 - Duplo
     (now() + INTERVAL '2 days', now() + INTERVAL '5 days', 2, 'Reservada',
-    'Hospedagem de férias', '56789012345', 2),
+    'Hospedagem de férias', '56789012345',
+    (
+        SELECT q.id_quarto
+        FROM quartos q
+        JOIN tipos_de_quartos t 
+            ON t.id_tipo_de_quarto = q.tipo_id
+        WHERE t.nome = 'Duplo'
+        AND q.status = 'Manutenção'
+    )),
 
-    (now() + INTERVAL '5 days', now() + INTERVAL '8 days', 4, 'Reservada',
-    'Viagem em família', '67890123456', 4),
+    -- Reserva 7 - Suite
+    (now() + INTERVAL '5 days', now() + INTERVAL '8 days', 2, 'Reservada',
+    'Viagem em família', '67890123456',
+    (
+        SELECT q.id_quarto
+        FROM quartos q
+        JOIN tipos_de_quartos t 
+            ON t.id_tipo_de_quarto = q.tipo_id
+        WHERE t.nome = 'Suite'
+        AND q.status = 'Disponível'
+    )),
 
+    -- Reserva 8 - Triplo
     (now() - INTERVAL '10 days', now() - INTERVAL '7 days', 2, 'Finalizada',
-    'Estadia de três dias', '78901234567', 3),
+    'Estadia de três dias', '78901234567',
+    (
+        SELECT q.id_quarto
+        FROM quartos q
+        JOIN tipos_de_quartos t 
+            ON t.id_tipo_de_quarto = q.tipo_id
+        WHERE t.nome = 'Triplo'
+        AND q.status = 'Disponível'
+    )),
 
+    -- Reserva 9 - Individual
     (now() - INTERVAL '4 days', now() - INTERVAL '2 days', 1, 'Finalizada',
-    'Hospedagem individual', '89012345678', 1),
+    'Hospedagem individual', '89012345678',
+    (
+        SELECT q.id_quarto
+        FROM quartos q
+        JOIN tipos_de_quartos t 
+            ON t.id_tipo_de_quarto = q.tipo_id
+        WHERE t.nome = 'Individual'
+        AND q.status = 'Manutenção'
+    )),
 
+    -- Reserva 10 - Triplo
     (now() + INTERVAL '7 days', now() + INTERVAL '10 days', 3, 'Reservada',
-    'Reserva para evento', '90123456789',4),
+    'Reserva para evento', '90123456789',
+    (
+        SELECT q.id_quarto
+        FROM quartos q
+        JOIN tipos_de_quartos t 
+            ON t.id_tipo_de_quarto = q.tipo_id
+        WHERE t.nome = 'Triplo'
+        AND q.status = 'Ocupado'
+    )),
 
+    -- Reserva 11 - Duplo
     (now() + INTERVAL '3 days', now() + INTERVAL '6 days', 2, 'Cancelada',
-    'Reserva cancelada pelo hóspede', '01234567890', 3),
+    'Reserva cancelada pelo hóspede', '01234567890',
+    (
+        SELECT q.id_quarto
+        FROM quartos q
+        JOIN tipos_de_quartos t 
+            ON t.id_tipo_de_quarto = q.tipo_id
+        WHERE t.nome = 'Duplo'
+        AND q.status = 'Disponível'
+    )),
 
+    -- Reserva 12 - Individual
     (now() - INTERVAL '2 days', now() + INTERVAL '1 day', 1, 'Hospedado',
-    'Hóspede em viagem de negócios', '00012320900', 2),
+    'Hóspede em viagem de negócios', '00012320900',
+    (
+        SELECT q.id_quarto
+        FROM quartos q
+        JOIN tipos_de_quartos t 
+            ON t.id_tipo_de_quarto = q.tipo_id
+        WHERE t.nome = 'Individual'
+        AND q.status = 'Disponível'
+    )),
 
+    -- Reserva 13 - Suite
     (now() + INTERVAL '10 days', now() + INTERVAL '13 days', 2, 'Reservada',
-    'Reserva de férias', '34567890123', 2),
+    'Reserva de férias', '34567890123',
+    (
+        SELECT q.id_quarto
+        FROM quartos q
+        JOIN tipos_de_quartos t 
+            ON t.id_tipo_de_quarto = q.tipo_id
+        WHERE t.nome = 'Suite'
+        AND q.status = 'Manutenção'
+    )),
 
-    (now() - INTERVAL '8 days', now() - INTERVAL '5 days', 4, 'Finalizada',
-    'Hospedagem em grupo', '45678901234', 3),
+    -- Reserva 14 - Triplo
+    (now() - INTERVAL '8 days', now() - INTERVAL '5 days', 3, 'Finalizada',
+    'Hospedagem em grupo', '45678901234',
+    (
+        SELECT q.id_quarto
+        FROM quartos q
+        JOIN tipos_de_quartos t 
+            ON t.id_tipo_de_quarto = q.tipo_id
+        WHERE t.nome = 'Triplo'
+        AND q.status = 'Ocupado'
+    )),
 
+    -- Reserva 15 - Individual
     (now() + INTERVAL '15 days', now() + INTERVAL '18 days', 1, 'Cancelada',
-    'Cancelamento solicitado pelo cliente', '00012320900', 1);
+    'Cancelamento solicitado pelo cliente', '00012320900',
+    (
+        SELECT q.id_quarto
+        FROM quartos q
+        JOIN tipos_de_quartos t 
+            ON t.id_tipo_de_quarto = q.tipo_id
+        WHERE t.nome = 'Individual'
+        AND q.status = 'Ocupado'
+    ));
 
 INSERT INTO servicos (nome, descricao, preco, situacao) VALUES
-    ('Café da Manhã', 'Café da manhã completo servido das 07:00 às 10:00.', 35.00, 'Ativo'),
-    ('Estacionamento', 'Vaga de estacionamento privativa para hóspedes.', 25.00, 'Ativo'),
-    ('Lavanderia', 'Serviço de lavagem e secagem de roupas dos hóspedes.', 40.00, 'Ativo'),
-    ('Traslado Aeroporto', 'Transporte entre o hotel e o aeroporto mediante agendamento.', 80.00, 'Ativo'),
-    ('Passeio Turístico', 'Passeio turístico pela cidade com guia especializado.', 120.00, 'Inativo'),
-    ('Serviço de Quarto', 'Serviço de alimentação e bebidas diretamente no quarto.', 15.00, 'Ativo');
+    ('Café da Manhã', 'Café da manhã completo servido das 07:00 às 10:00.', 35.00, true),
+    ('Estacionamento', 'Vaga de estacionamento privativa para hóspedes.', 25.00, true),
+    ('Lavanderia', 'Serviço de lavagem e secagem de roupas dos hóspedes.', 40.00, true),
+    ('Traslado Aeroporto', 'Transporte entre o hotel e o aeroporto mediante agendamento.', 80.00, true),
+    ('Passeio Turístico', 'Passeio turístico pela cidade com guia especializado.', 120.00, false),
+    ('Serviço de Quarto', 'Serviço de alimentação e bebidas diretamente no quarto.', 15.00, true);
 
 INSERT INTO servicos_reservas (servico_id,reserva_id,quantidade_utilizada) VALUES
     (1,3,2),
