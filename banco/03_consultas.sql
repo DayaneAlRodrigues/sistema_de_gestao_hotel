aedcd10ad706708210bf06fe14b8d2285410dca9
Dayane Rodrigues <dayanealrodrigues@gmail.com>
2026-08-19 11:19:05 -0300

    Consulta Quartos Ocupados

--- banco/03_consultas.sql
+++ banco/03_consultas.sql
@@ -21,4 +21,33 @@ ON quartos.id_quarto = reservas.quarto_id
 INNER JOIN tipos_de_quartos AS tipo
 ON tipo.id_tipo_de_quarto = quartos.tipo_id
 
-ORDER BY reservas.data_entrada;
+ORDER BY reservas.data_entrada ASC;
+
+--Quartos ocupados
+--Apresentar os quartos ocupados em determinada data, com quarto, tipo, hóspede, entrada e saída.
+
+SELECT 
+    quartos.id_quarto AS quarto,
+    tipo.nome AS tipo_do_quarto,
+    hospedes.nome AS hospede,
+    reservas.data_entrada,
+    reservas.data_saida
+FROM reservas
+
+INNER JOIN  hospedes
+ON hospedes.cpf = reservas.hospede_cpf
+
+INNER JOIN quartos
+ON quartos.id_quarto = reservas.quarto_id
+
+INNER JOIN tipos_de_quartos AS tipo
+ON tipo.id_tipo_de_quarto = quartos.tipo_id
+
+WHERE quartos.status = 'Ocupado'
+AND  reservas.data_entrada <= '2026-08-12'
+AND reservas.data_saida >= '2026-08-15'
+
+ORDER BY reservas.data_entrada ASC;
+
+
+    
